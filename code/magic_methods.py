5b2cab4fbe1fc539982f91d8463df105c0cb2cfa
noresources <dennis.petta@gmail.com>
2020-01-01 16:56:49 +0200

    topic division and added material

--- code/magic_methods.py
+++ code/magic_methods.py
@@ -8,11 +8,14 @@ class Polynomial:
 
     # Notion of addition
     def __add__(self, other):
-        return Polynomial(*(x + y) for x, y in zip(self.coeffs, other.coeffs))
+        return Polynomial(*((x + y) for x, y in zip(self.coeffs, other.coeffs)))
 
 
-# p1 = Polynomial(1, 2, 3) # x^2 + 2x + 3
-# p2 = Polynomial(3, 4, 5) # 3x^2 + 4x + 3
 
-# print(p1)
-# print(p1 + p2)
+p1 = Polynomial(1, 2, 3) # x^2 + 2x + 3
+p2 = Polynomial(3, 4, 5) # 3x^2 + 4x + 3
+
+print(p1 + p2 + p2)
+
+
+
