1dc6d48791373e73fc1b9bfd3c6359f519fd65e5
noresources <dennis.petta@gmail.com>
2020-01-02 17:58:43 +0200

    added second example

--- code/magic_methods.py
+++ code/magic_methods.py
@@ -17,5 +17,43 @@ p2 = Polynomial(3, 4, 5) # 3x^2 + 4x + 3
 
 print(p1 + p2 + p2)
 
+class Angle:
+    def __init__(self, degrees):
+        # Wrap it around so that the value is always
+        # in the interval of 0 and 359.999...
+        self.degrees = degrees % 360
+    # Less-than
+    def __lt__(self, other):
+        return self.degrees < other.degrees
+    # Less-than-or-equals
+    def __le__(self, other):
+        return self.degrees <= other.degrees
+    # Greater-than
+    def __gt__(self, other):
+        return self.degrees > other.degrees
+    # Greater-than-or-equals
+    def __ge__(self, other):
+        return self.degrees >= other.degrees
+    # Equals
+    def __eq__(self, other):
+        return self.degrees == other.degrees
+    
+    
 
+
+class my_object(object):
+    
+    def __init__(self):
+        pass
+    
+    def __repr__(self):
+        return f"representation of the object: {self.__class__}"
+    
+    def __str__(self):
+        return f"string format of the object"
+    
+obj = my_object()
+
+print(f"With repr you get {repr(obj)}")
+print(f"With str you get {str(obj)}")
 
